a298746ca8f4613eeded8758a88b384dd406d3b7
emlia <771631347@qq.com>
2020-04-07 14:34:57 +0800

    add vue-mvvm , debounce

---
+++ code/debounce/debounce.js
@@ -0,0 +1,24 @@
+const debounce = (func, time = 17, optoins = {
+  leading: true, context: null
+}) => {
+  let timer;
+  const _debounce = function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    if (optoins.leading && !timer) {
+      timer = setTimeout(null, time);
+      func.apply(optoins.context, args)
+    } else {
+      timer = setTimeout(() => {
+        func.apply(optoins.context, args)
+        timer = null
+      }, time);
+    }
+  }
+  _debounce.cancel = function () {
+    clearTimeout(timer)
+    timer = null
+  }
+  return _debounce
+}
